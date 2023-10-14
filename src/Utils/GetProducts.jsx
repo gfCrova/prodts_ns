1f156bdf51c7fcf510ba4a05723c853f6799e657
gfCrova <giancrova.cl5@gmail.com>
2023-10-13 22:29:53 -0300

    add Utils folder, and fetch of the api products

---
+++ src/Utils/GetProducts.jsx
@@ -0,0 +1,27 @@
+
+export const getProducts = async () => {
+
+    try {
+        const results = await fetch('https://api.escuelajs.co/api/v1/products');
+        const data = await results.json();
+        return data;
+      } catch (error) {
+        console.error("Error al obtener los productos:", error);
+        throw error; // Puedes manejar el error aquí o relanzarlo para que lo maneje quien llame a esta función.
+      }
+}
+
+export const productos = async () => {
+    try {
+        const productsData = await getProducts();
+        const productTitles = productsData.map(prod => prod);
+        return productTitles;
+      } catch (error) {
+        // Maneja cualquier error que ocurra al obtener los productos o al procesar los títulos.
+        console.error("Error al obtener los títulos de los productos:", error);
+        throw error; // Puedes manejar el error aquí o relanzarlo para que lo maneje quien llame a esta función.
+      }
+};
+
+
+export default getProducts
