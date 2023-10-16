003ec749538dcce5ad531718a48f432490b9798a
gfCrova <giancrova.cl5@gmail.com>
2023-10-16 11:22:06 -0300

    deleted components whit out renamereds

--- src/components/BotonComprar.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './All.module.css'
-
-function BotonComprar() {
-  return (
-    <button className={`${styles.botonComprar}`}> Comprar </button>
-  )
-}
-
-export default BotonComprar
--- src/components/Title.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './All.module.css'
-
-const Title = () => {
-        return (
-            <h1 className={`${styles.title}`}>Tienda</h1>
-        );
-    }
-
-export default Title;
